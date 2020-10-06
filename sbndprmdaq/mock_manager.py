6255e23594d403acbfa9f320054a67d10d16fec3
Marco Del Tutto <marco.deltutto@gmail.com>
2020-10-06 10:56:26 -0500

    Update mock_manager.py

--- sbndprmdaq/mock_manager.py
+++ sbndprmdaq/mock_manager.py
@@ -1,4 +1,4 @@
-# from sbndprmdaq.digitizer import ATS310
+from sbndprmdaq.digitizer.mock_ats310 import MockATS310
 from sbndprmdaq.parallel_communication.mock_communicator import MockCommunicator
 
 
@@ -6,6 +6,7 @@ class MockPrMManager():
 
     def __init__(self):
         print('Hello!')
+        self._ats310 = MockATS310()
         self._comm = MockCommunicator()
 
 
@@ -13,11 +14,19 @@ class MockPrMManager():
 
         print('ok, tested')
 
+    def digitizer_busy(self):
+        '''
+        Returns the digitizers status
+        (if it is busy or now)
+        '''
+        return self._ats310.busy()
 
-     def start_prm(self):
+
+    def start_prm(self):
         '''
         Sets the parallel port pin that turns the PrM ON
         '''
+        self._ats310.start_capture()
         self._comm.start_prm()
 
 
