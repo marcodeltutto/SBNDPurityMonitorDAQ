be1c06b8e93be0c82a4279bc59a01b6297b34ebd
AlexWilkinsonn <alexander.wilkinson.20@ucl.ac.uk>
2024-03-13 23:04:55 +0000

    analysis class that fits waveforms

--- sbndprmdaq/analysis.py
+++ sbndprmdaq/analysis.py
@@ -4,9 +4,9 @@ Contains PrM analysis class
 
 import datetime
 import subprocess
-import numpy as np
 from abc import ABC, abstractmethod
 
+import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal; import scipy.optimize
 
@@ -46,14 +46,6 @@ class PrMAnalysisBase(ABC):
         self._wf_a = None
         self._wf_x = None
 
-        self._time_start_c = None
-        self._time_end_c = None
-        self._deltat_c = None
-
-        self._time_start_a = None
-        self._time_end_a = None
-        self._deltat_a = None
-
         self._qa = None
         self._qc = None
 
@@ -80,7 +72,6 @@ class PrMAnalysisBase(ABC):
         self._plot_range = config.get('plot_range', [0, 3500])
         self._plot_title = config.get('title', 'PrM')
 
-        # XXX can we not know the size of the readout beforehand?
         if len(self._raw_wf_c) == 6000:
             self._signal_range_c = [i * 2 for i in self._signal_range_c]
             self._signal_range_a = [i * 2 for i in self._signal_range_a]
@@ -186,10 +177,14 @@ class PrMAnalysisBase(ABC):
             code = -11
         elif status == 'cathode_deltat_large':
             code = -12
-        elif code == 'anode_deltat_large':
+        elif status == 'anode_deltat_large':
             code = -13
-        elif code == 'qa_lt_qc':
+        elif status == 'qa_lt_qc':
             code = -14
+        elif status == 'fit_failed':
+            code = -20
+        elif status == 'deltat_negative':
+            code = -21
         else:
             code = -99
 
@@ -234,6 +229,14 @@ class PrMAnalysisEstimate(PrMAnalysisBase):
         self._baseline_rms_c = None
         self._baseline_a = None
         self._baseline_rms_a = None
+
+        self._time_start_c = None
+        self._time_end_c = None
+        self._deltat_c = None
+
+        self._time_start_a = None
+        self._time_end_a = None
+        self._deltat_a = None
 
         self._offset = None
 
@@ -347,7 +350,7 @@ class PrMAnalysisEstimate(PrMAnalysisBase):
             fontsize=12
         )
 
-        self.set_lifetime_axis(ax, self._plot_title, container, text_pos=[0.015, 0.56])
+        self._set_lifetime_axis(ax, self._plot_title, container, text_pos=[0.015, 0.56])
 
         if savename:
             plt.savefig(savename)
@@ -444,7 +447,7 @@ class PrMAnalysisEstimate(PrMAnalysisBase):
             self._time_end_a = end
             self._deltat_a = end - start[0]
         except:
-            return 'deltat_cathode_failed'
+            return 'deltat_anode_failed'
 
         if self._debug: print('_estimate_deltat done')
         return 'ok'
@@ -587,7 +590,7 @@ class PrMAnalysisEstimate(PrMAnalysisBase):
 
 class PrMAnalysisFitter(PrMAnalysisBase):
     '''
-    A class to PrM analysis by trying to fit waveforms
+    A class to perform PrM analysis by fitting waveforms, See arXiv:2005.08187 for details.
     '''
 
     def __init__(
@@ -615,30 +618,199 @@ class PrMAnalysisFitter(PrMAnalysisBase):
             debug=debug
         )
 
+        self._samples_per_sec = samples_per_sec
+
+        self._wf_x = self._raw_wf_x
+
+        self._baseline_c_pre = None
+        self._baseline_c_pre_err = None
+        self._baseline_c_post = None
+        self._baseline_c_post_err = None
+        self._baseline_a_pre = None
+        self._baseline_a_pre_err = None
+        self._baseline_a_post = None
+        self._baseline_a_post_err = None
+
+        self._t_start_c = None
+        self._t_start_c_err = None
+        self._t_rise_c = None
+        self._t_rise_c_err = None
+        self._t_start_a = None
+        self._t_start_a_err = None
+        self._t_rise_a = None
+        self._t_rise_a_err = None
+
+        self._t_c_cgrid = None
+        self._t_c_cgrid_err = None
+        self._t_cgrid_agrid = None
+        self._t_cgrid_agrid_err = None
+        self._t_agrid_a = None
+        self._t_agrid_a_err = None
+
+        self._qa_err = None
+        self._qc_err = None
+
+        self._td_err = None
+        self._tau_err = None
+
         self._lowpass_cutoff_freq = config.get('lowpass_cutoff_freq', 100e3)
+        self._a_fit_start_offset = config.get('anode_fit_start_offset', 500)
 
     def calculate(self):
         '''
         Performs all the calculation
         '''
-        self._pre_process(self._low_pass_cut_off_freq)
+        self._err = self._pre_process(self._lowpass_cutoff_freq)
+        if self._err != 'ok':
+            self._process_error(self._err)
+            return
 
-    def _pre_process(self, cut_off_freq):
+        self._err = self._fit_cathode()
+        if self._err != 'ok':
+            self._process_error(self._err)
+            return
+
+        self._err = self._fit_anode()
+        if self._err != 'ok':
+            self._process_error(self._err)
+            return
+
+        self._err = self._calculate_delta_ts()
+        if self._err != 'ok':
+            self._process_error(self._err)
+            return
+
+        self._err = self._calculate_lifetime()
+        if self._err != 'ok':
+            self._process_error(self._err)
+            return
+
+    def plot_summary(self, container=None, savename=None):
+        '''
+        Generates a summary plot
+
+        Args:
+            container (dict): data container to add info on plot
+            savename (string): full path to save file
+        '''
+        if self._td is None:
+            return None, None
+
+        if self._td < 0:
+            return None, None
+
+        prop_cycle = plt.rcParams['axes.prop_cycle']
+        _colors = prop_cycle.by_key()['color']
+
+        fig, ax = plt.subplots(ncols=1, nrows=2, figsize=(12, 8), sharex=True)
+        fig.subplots_adjust(hspace=0)
+
+        ax[0].plot(
+            self._raw_wf_x, self._raw_wf_a, label='Anode Raw', color=_colors[0], alpha=0.5
+        )
+        ax[1].plot(
+            self._raw_wf_x, self._raw_wf_c, label='Cathode Raw', color=_colors[1], alpha=0.5
+        )
+        ax[0].plot(self._wf_x, self._wf_a, label='Anode', color=_colors[0])
+        ax[1].plot(self._wf_x, self._wf_c, label='Cathode', color=_colors[1])
+
+        print(
+            self._wf_x,
+            self._baseline_a_pre, self._baseline_a_post,
+            self._qa,
+            self._t_start_a, self._t_rise_a
+        )
+        ax[0].plot(
+            self._wf_x,
+            self._fit_func(
+                self._wf_x,
+                self._baseline_a_pre, self._baseline_a_post,
+                self._qa,
+                self._t_start_a, self._t_rise_a
+            ),
+            label='Anode Fit',
+            c='k'
+        )
+        ax[1].plot(
+            self._wf_x,
+            self._fit_func(
+                self._wf_x,
+                self._baseline_c_pre, self._baseline_c_post,
+                -self._qc,
+                self._t_start_c, self._t_rise_c
+            ),
+            label='Cathode Fit',
+            c='k'
+        )
+
+        ax[0].axvline(self._t_start_c, color='grey', linestyle='dashed')
+        ax[1].axvline(self._t_start_c, color='grey', linestyle='dashed')
+        ax[0].axvline(self._t_rise_a, color='grey', linestyle='dashed')
+        ax[1].axvline(self._t_rise_a, color='grey', linestyle='dashed')
+
+        ax[0].axvspan(self._t_start_a, self._t_rise_a, alpha=0.5, color=_colors[0])
+        ax[1].axvspan(self._t_start_c, self._t_rise_c, alpha=0.5, color=_colors[1])
+
+        # ax[0].axhline(
+        #     self._max_a, color=_colors[0], label=f'Max = {self._max_a:.1f} mV', linestyle='dashdot'
+        # )
+        # ax[1].axhline(
+        #     self._max_c, color=_colors[1], label=f'Max = {self._max_c:.1f} mV', linestyle='dashdot'
+        # )
+
+        x_plot_range = self._t_rise_a * 2
+        ax[0].set_xlim([0, x_plot_range])
+        ax[1].set_xlim([0, x_plot_range])
+
+        y_plot_range = np.abs(self._qc) * 1.4
+        ax[0].set_ylim([-29.99, y_plot_range])
+        ax[1].set_ylim([-y_plot_range, 29.99])
+
+        ax[0].set_title(
+            (
+                f'Drift time: {self._td/1e3:.2f} ' + r'$ms$'
+                f'\nQa/Qc: {self._qa/self._qc:.2f}'
+                f'\nLifetime: {self._tau/1e3:.2f} ' + r'$ms$'
+            ),
+            loc='left',
+            fontsize=12
+        )
+
+        # self._set_lifetime_axis(ax, self._plot_title, container, text_pos=[0.015, 0.56])
+
+        if savename:
+            plt.savefig(savename)
+        plt.show()
+
+        return fig, ax
+
+    def _pre_process(self, cutoff_freq):
         '''
         Applies a low-pass filter to the raw waveform
 
         Args:
-            cut_off_freq (float): cut-off frequency for low-pass filter
+            cutoff_freq (float): cut-off frequency for low-pass filter
         '''
-        nyquist_freq = self.samples_per_sec / 2
-        crit_freq = cut_off_freq / nyquist_freq # normalise the cut-off
+        nyquist_freq = self._samples_per_sec / 2
+        crit_freq = cutoff_freq / nyquist_freq # normalise the cut-off
         b, a = scipy.signal.butter(3, crit_freq)
         self._wf_c = scipy.signal.filtfilt(b, a, self._raw_wf_c)
         self._wf_a = scipy.signal.filtfilt(b, a, self._raw_wf_a)
 
-    def _fit_func(self, t, baseline, V_0, t_start, t_rise):
+        return 'ok'
+
+    def _fit_func(self, t, baseline_pre, baseline_post, V_0, t_start, t_rise):
         '''
-        Fit function for PrM waveforms
+        Fit function for PrM waveforms. Applied to cathode and anode signal separately.
+        Two baselines are useful since baseline can sometimes change after discharge.
+
+        Args:
+            t (float): time variable
+            baseline_pre (float): parameter
+            baseline_post (float): parameter
+            V_0 (float): parameter
+            t_start (float): parameter
+            t_rise (float): parameter
         '''
         return np.piecewise(
             t,
@@ -649,18 +821,219 @@ class PrMAnalysisFitter(PrMAnalysisBase):
             ],
             [
                 lambda t: (
-                    baseline
+                    baseline_pre
                 ),
                 lambda t: (
                     V_0 * (1 - np.exp(-(t - t_start) / self._RC)) / ((t_rise - t_start) / self._RC) + # charging
-                    baseline
+                    baseline_pre
                 ),
                 lambda t: (
-                    (V_0 * (1 - np.exp(-(t_rise - t_start) / self._RC)) / ((t_rise - t_start) / self._RC)) *
-                    np.exp(-(t - t_rise) / self._RC) + # discharging from V_max
-                    baseline
+                    (
+                        (V_0 * (1 - np.exp(-(t_rise - t_start) / self._RC)) / ((t_rise - t_start) / self._RC)) + # V max
+                        (baseline_pre - baseline_post) # smoothly go to new baseline
+                    ) *
+                    np.exp(-(t - t_rise) / self._RC) + # discharging from V max
+                    baseline_post
                 )
             ]
         )
+
+    def _fit_cathode(self):
+        '''
+        Apply fitting function to cathode.
+        '''
+        cathode_p_guess = [
+            np.mean(self._wf_c[self._baseline_range_c[0]:self._baseline_range_c[1]]), # baseline_pre
+            np.mean(self._wf_c[self._baseline_range_c[0]:self._baseline_range_c[1]]), # baseline_post
+            np.min(self._wf_c), # V_0
+            self._wf_x[np.argmin(self._wf_c) - 100], # t_start
+            self._wf_x[np.argmin(self._wf_c)] # t_rise
+        ]
+
+        try:
+            popt_cathode, pcov_cathode = scipy.optimize.curve_fit(
+                self._fit_func, self._wf_x, self._wf_c,
+                p0=cathode_p_guess,
+                bounds=(
+                    [-np.inf, -np.inf, -np.inf, 0.0, 0.0],
+                    [np.inf, np.inf, 0.0, max(self._wf_x), max(self._wf_x)]
+                )
+            )
+        except Exception as e:
+            print(e)
+            return 'fit_failed'
+
+        self._baseline_c_pre, self._baseline_c_post, self._qc, self._t_start_c, self._t_rise_c = (
+            popt_cathode
+        )
+        self._qc = -self._qc
+        _, _, self._qc_err, self._t_start_c_err, self._t_rise_c_err = (
+            np.sqrt(np.diag(pcov_cathode))
+        )
+
+        if self._debug:
+            print(np.column_stack([
+                ['baseline_c_pre', 'baseline_c_post', 'qc', 't_start_c', 't_rise_c'],
+                popt_cathode,
+                np.sqrt(np.diag(pcov_cathode))
+            ]))
+
+        return 'ok'
+
+    def _fit_anode(self):
+        '''
+        Apply fitting function to anode.
+        '''
+        anode_max_tick = np.argmax(self._wf_a)
+        anode_p_guess = [
+            np.mean(self._wf_a[self._baseline_range_a[0]:self._baseline_range_a[1]]), # baseline
+            np.mean(self._wf_a[self._baseline_range_a[0]:self._baseline_range_a[1]]), # baseline
+            np.max(self._wf_a), # V_0
+            self._wf_x[anode_max_tick - 100], # t_start
+            self._wf_x[anode_max_tick] # t_rise
+        ]
+
+        try:
+            popt_anode, pcov_anode = scipy.optimize.curve_fit(
+                self._fit_func,
+                self._wf_x[anode_max_tick - self._a_fit_start_offset:],
+                self._wf_a[anode_max_tick - self._a_fit_start_offset:],
+                p0=anode_p_guess,
+                bounds=(
+                    [-np.inf, -np.inf, 0.0, 0.0, 0.0],
+                    [np.inf, np.inf, np.inf, max(self._wf_x), max(self._wf_x)]
+                )
+            )
+        except Exception as e:
+            print(e)
+            return 'fit_failed'
+
+        self._baseline_a_pre, self._baseline_a_post, self._qa, self._t_start_a, self._t_rise_a = (
+            popt_anode
+        )
+        _, _, self._qa_err, self._t_start_a_err, self._t_rise_a_err = (
+            np.sqrt(np.diag(pcov_anode))
+        )
+
+        if self._debug:
+            print(np.column_stack([
+                ['baseline_a_pre', 'baseline_a_post', 'qa', 't_start_a', 't_rise_a'],
+                popt_anode,
+                np.sqrt(np.diag(pcov_anode))
+            ]))
+
+        return 'ok'
+
+    def _calculate_delta_ts(self):
+        '''
+        Calculate and check time differences needed for lifetime calculation
+        '''
+        self._t_c_cgrid = self._t_rise_c - self._t_start_c
+        self._t_c_cgrid_err = np.sqrt(self._t_rise_c_err**2 + self._t_start_c_err**2)
+
+        self._t_cgrid_agrid = self._t_start_a - self._t_rise_c
+        self._t_cgrid_agrid_err = np.sqrt(self._t_start_a_err**2 + self._t_rise_c_err**2)
+
+        self._t_agrid_a = self._t_rise_a - self._t_start_a
+        self._t_agrid_a_err = np.sqrt(self._t_rise_a_err**2 + self._t_start_a_err**2)
+
+        if self._t_c_cgrid < 0 or self._t_cgrid_agrid < 0 or self._t_agrid_a < 0:
+            return 'deltat_negative'
+
+        if self._debug:
+            print(np.column_stack([
+                ['t_c_cgrid', 't_cgrid_agrid', 't_agrid_a'],
+                [self._t_c_cgrid, self._t_cgrid_agrid, self._t_agrid_a],
+                [self._t_c_cgrid_err, self._t_cgrid_agrid_err, self._t_agrid_a_err],
+            ]))
+
+        return 'ok'
+
+    def _calculate_lifetime(self):
+        '''
+        Lifetime calculation.
+        '''
+        R = self._qa / self._qc
+        R_err = R * np.sqrt((self._qa_err / self._qa)**2 + (self._qc_err / self._qc)**2)
+
+        self._tau = (-1 / np.log(R)) * (self._t_cgrid_agrid + (self._t_c_cgrid + self._t_agrid_a) / 2)
+        self._tau_err = (
+            self._tau * np.sqrt(
+                ((1 / np.log(R)) * (R_err / R))**2 +
+                (
+                    np.sqrt(
+                        self._t_cgrid_agrid_err**2 +
+                        (self._t_c_cgrid_err / 2)**2 +
+                        (self._t_agrid_a_err / 2)**2
+                    ) /
+                    (self._t_cgrid_agrid + (self._t_c_cgrid + self._t_agrid_a) / 2)
+                )**2
+            )
+        )
+
+        # Just for consistency, not used in the lifetime calculation
+        self._td = self._t_rise_a - self._t_start_c
+        self._td_err = np.sqrt(self._t_rise_a_err**2 + self._t_start_c_err**2)
+
+        if self._debug:
+            print(np.column_stack([
+                ['R', 'tau', 'td'], [R, self._tau, self._td], [R, self._tau_err, self._td_err]
+            ]))
+
+        return 'ok'
+
+    def _set_lifetime_axis(self, ax, title, container=None, draw_txt=True, text_pos=None):
+        '''
+        Customizes the axis
+        '''
+        for a in ax:
+            a.set_ylabel('Waveform [mV]',fontsize=18)
+            a.set_xlabel('Time [us]',fontsize=18)
+        #     a.set_title('SBND Purity Monitors', loc='right', fontsize=18)
+            a.tick_params(labelsize=15)
+            a.grid(True)
+
+        ax[0].legend(fontsize=12, loc=1)
+        ax[1].legend(fontsize=12, loc=4)
+
+        # ax[0].set_ylim([-29.9999, 30])
+        # ax[1].set_ylim([-30,    29.9999])
+
+        ax[0].set_title(title, loc='right', fontsize=18)
+
+        if draw_txt and container is not None:
+            self._draw_text_lifetime(ax[0], container, text_pos)
+
+    def _draw_text_lifetime(self, ax, container, pos=None):
+        '''
+        Draw info text on plot
+        '''
+        date = datetime.datetime.strptime(str(container['date']), '%Y%m%d-%H%M%S')
+
+        short_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
+        textstr = '\n'.join((
+            r'Run %i' % (container['run'], ),
+            r'%s' % (date.strftime("%B %d, %Y - %H:%M"), ),
+            r'Cathode HV = %.0f V' % (container['hv_cathode'], ),
+            r'Anode Grid HV = %.0f V' % (container['hv_anodegrid'], ),
+            r'Anode HV = %.0f V' % (container['hv_anode'], ),
+            r'n traces = %.0i' % (len(container['ch_B']), ),
+        ))
+
+        if pos is None:
+            pos = [0.98, 0.15]
+
+        props = {"boxstyle": 'round', "edgecolor": 'grey', "facecolor": 'white', "alpha": 0.8,}
+        ax.text(pos[0], pos[1], textstr, transform=ax.transAxes, fontsize=12,
+                verticalalignment='bottom',
+                horizontalalignment='left',
+                bbox=props)
+
+        props = {"boxstyle": 'round', "edgecolor": 'white', "facecolor": 'white', "alpha": 0,}
+        ax.text(0.6, 1, 'SBNDPurityMonitorDAQ: ' + short_hash, transform=ax.transAxes, fontsize=8,
+                verticalalignment='bottom',
+                horizontalalignment='right',
+                bbox=props)
 
 
